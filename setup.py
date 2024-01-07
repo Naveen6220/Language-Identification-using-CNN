dd7b41b803e85b347c2c72afe5a3c939156ab239
Narayanan <146112960+Naveen6220@users.noreply.github.com>
2024-01-07 18:14:35 +0530

    second commit

---
+++ setup.py
@@ -0,0 +1,8 @@
+from setuptools import find_packages, setup
+
+
+setup(name="Language_Identification_Using_CNN",
+    author_name="Aravind",
+    author_email="aravind@ineuron.ai",
+    packages=find_packages(),
+    install_requires=[])
